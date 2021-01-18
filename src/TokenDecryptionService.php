28cd8e92532be98c24ce22d25e20c6f3ddf50fa6
jongwoo Yoo <dbwhddn10@gmail.com>
2021-01-18 15:20:50 +0900

    first commit

---
+++ src/TokenDecryptionService.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace FnService\JWT;
+
+use Exception;
+use FnService\Service;
+use Jose\Component\Core\AlgorithmManager;
+use Jose\Component\Core\JWK;
+use Jose\Component\Encryption\Algorithm\ContentEncryption\A128CBCHS256;
+use Jose\Component\Encryption\Algorithm\KeyEncryption\A128GCMKW;
+use Jose\Component\Encryption\Compression\CompressionMethodManager;
+use Jose\Component\Encryption\Compression\Deflate;
+use Jose\Component\Encryption\JWEDecrypter;
+use Jose\Component\Encryption\Serializer\CompactSerializer;
+
+class TokenDecryptionService extends Service
+{
+    public static function getArrBindNames() : array
+    {
+        return [
+            'valid_token'
+                => 'valid {{token}}',
+        ];
+    }
+
+    public static function getArrCallbackLists() : array
+    {
+        return [];
+    }
+
+    public static function getArrLoaders() : array
+    {
+        return [
+            'decrypter' => function () {
+
+                $keyEncryptionAlgorithmManager = new AlgorithmManager([
+                    new A128GCMKW,
+                ]);
+                $contentEncryptionAlgorithmManager = new AlgorithmManager([
+                    new A128CBCHS256,
+                ]);
+                $compressionMethodManager = new CompressionMethodManager([
+                    new Deflate,
+                ]);
+
+                return new JWEDecrypter(
+                    $keyEncryptionAlgorithmManager,
+                    $contentEncryptionAlgorithmManager,
+                    $compressionMethodManager
+                );
+            },
+
+            'jwk' => function () {
+
+                return new JWK([
+                    'alg'
+                        => 'A128GCMKW',
+                    'use'
+                        => 'enc',
+                    'kty'
+                        => 'oct',
+                    'k'
+                        => 'I2FeeR3Th6FmhgHN-cxd-9GRRiwcNB2OQzW6vouGFd5hcAwNAu1377hvDmGLKttBitlHiFzk643FyHw4XFM9tdJ90s2zmkX3SsE2KX5B1Qe_sEhqYmWZsJyjeyx-Q0w4B2jX7b39GUybimHUoVHDPTUrgPUKeBf-xVIGJCvHyiE'
+                ]);
+            },
+
+            'payload' => function ($decrypter, $jwk, $token) {
+
+                $jwe = (new CompactSerializer)->unserialize($token);
+
+                $decrypter->decryptUsingKey($jwe, $jwk, 0);
+
+                return json_decode($jwe->getPayLoad(), true);
+            },
+
+            'payload_keys' => function () {
+
+                throw new Exception;
+            },
+
+            'result' => function ($payload) {
+
+                return $payload;
+            },
+
+            'valid_token' => function ($decrypter, $jwk, $payloadKeys, $token) {
+
+                try
+                {
+                    $jwe = (new CompactSerializer)->unserialize($token);
+
+                    $decrypter->decryptUsingKey($jwe, $jwk, 0);
+                }
+                /** @noinspection PhpUnusedLocalVariableInspection */
+                catch( Exception $exception )
+                {
+                    return null;
+                }
+
+                $isValid = true;
+                $payload = json_decode($jwe->getPayLoad(), true);
+
+                if ( $payload == null )
+                {
+                    return null;
+                }
+
+                foreach ( $payloadKeys as $key )
+                {
+                    if ( ! array_key_exists($key, $payload) )
+                    {
+                        $isValid = false;
+                    }
+                }
+
+                return $isValid ? $token : null;
+            },
+        ];
+    }
+
+    public static function getArrPromiseLists() : array
+    {
+        return [
+            'payload'
+                => ['valid_token:strict'],
+        ];
+    }
+
+    public static function getArrRuleLists() : array
+    {
+        return [
+            'token'
+                => ['required'],
+
+            'valid_token'
+                => ['required']
+        ];
+    }
+
+    public static function getArrTraits() : array
+    {
+        return [];
+    }
+}
