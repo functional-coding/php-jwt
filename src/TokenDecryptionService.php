1178ee5a218050e4f68741993c37b8a723221993
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-09 03:04:03 +0900

    add packages and files is changed

--- src/TokenDecryptionService.php
+++ src/TokenDecryptionService.php
@@ -1,72 +1,28 @@
 <?php
 
-namespace FnService\JWT;
+namespace FunctionalCoding\JWT;
 
-use Exception;
-use FnService\Service;
-use Jose\Component\Core\AlgorithmManager;
-use Jose\Component\Core\JWK;
-use Jose\Component\Encryption\Algorithm\ContentEncryption\A128CBCHS256;
-use Jose\Component\Encryption\Algorithm\KeyEncryption\A128GCMKW;
-use Jose\Component\Encryption\Compression\CompressionMethodManager;
-use Jose\Component\Encryption\Compression\Deflate;
-use Jose\Component\Encryption\JWEDecrypter;
-use Jose\Component\Encryption\Serializer\CompactSerializer;
+use FunctionalCoding\Service;
 
 class TokenDecryptionService extends Service
 {
-    public static function getArrBindNames() : array
+    public static function getArrBindNames()
     {
         return [
-            'valid_token'
-                => 'valid {{token}}',
+            'valid_token' => 'valid {{token}}',
         ];
     }
 
-    public static function getArrCallbacks() : array
+    public static function getArrCallbacks()
     {
         return [];
     }
 
-    public static function getArrLoaders() : array
+    public static function getArrLoaders()
     {
         return [
-            'decrypter' => function () {
-
-                $keyEncryptionAlgorithmManager = new AlgorithmManager([
-                    new A128GCMKW,
-                ]);
-                $contentEncryptionAlgorithmManager = new AlgorithmManager([
-                    new A128CBCHS256,
-                ]);
-                $compressionMethodManager = new CompressionMethodManager([
-                    new Deflate,
-                ]);
-
-                return new JWEDecrypter(
-                    $keyEncryptionAlgorithmManager,
-                    $contentEncryptionAlgorithmManager,
-                    $compressionMethodManager
-                );
-            },
-
-            'jwk' => function () {
-
-                return new JWK([
-                    'alg'
-                        => 'A128GCMKW',
-                    'use'
-                        => 'enc',
-                    'kty'
-                        => 'oct',
-                    'k'
-                        => 'I2FeeR3Th6FmhgHN-cxd-9GRRiwcNB2OQzW6vouGFd5hcAwNAu1377hvDmGLKttBitlHiFzk643FyHw4XFM9tdJ90s2zmkX3SsE2KX5B1Qe_sEhqYmWZsJyjeyx-Q0w4B2jX7b39GUybimHUoVHDPTUrgPUKeBf-xVIGJCvHyiE'
-                ]);
-            },
-
             'payload' => function ($decrypter, $jwk, $token) {
-
-                $jwe = (new CompactSerializer)->unserialize($token);
+                $jwe = (new CompactSerializer())->unserialize($token);
 
                 $decrypter->decryptUsingKey($jwe, $jwk, 0);
 
@@ -74,41 +30,33 @@ class TokenDecryptionService extends Service
             },
 
             'payload_keys' => function () {
-
-                throw new Exception;
+                throw new \Exception();
             },
 
             'result' => function ($payload) {
-
                 return $payload;
             },
 
             'valid_token' => function ($decrypter, $jwk, $payloadKeys, $token) {
-
-                try
-                {
-                    $jwe = (new CompactSerializer)->unserialize($token);
+                try {
+                    $jwe = (new CompactSerializer())->unserialize($token);
 
                     $decrypter->decryptUsingKey($jwe, $jwk, 0);
                 }
-                /** @noinspection PhpUnusedLocalVariableInspection */
-                catch( Exception $exception )
-                {
+                // @noinspection PhpUnusedLocalVariableInspection
+                catch (\Exception $exception) {
                     return null;
                 }
 
                 $isValid = true;
                 $payload = json_decode($jwe->getPayLoad(), true);
 
-                if ( $payload == null )
-                {
+                if (null == $payload) {
                     return null;
                 }
 
-                foreach ( $payloadKeys as $key )
-                {
-                    if ( ! array_key_exists($key, $payload) )
-                    {
+                foreach ($payloadKeys as $key) {
+                    if (!array_key_exists($key, $payload)) {
                         $isValid = false;
                     }
                 }
@@ -118,26 +66,23 @@ class TokenDecryptionService extends Service
         ];
     }
 
-    public static function getArrPromiseLists() : array
+    public static function getArrPromiseLists()
     {
         return [
-            'payload'
-                => ['valid_token:strict'],
+            'payload' => ['valid_token:strict'],
         ];
     }
 
-    public static function getArrRuleLists() : array
+    public static function getArrRuleLists()
     {
         return [
-            'token'
-                => ['required'],
+            'token' => ['required'],
 
-            'valid_token'
-                => ['required']
+            'valid_token' => ['required'],
         ];
     }
 
-    public static function getArrTraits() : array
+    public static function getArrTraits()
     {
         return [];
     }
--- src/TokenEncryptionService.php
+++ src/TokenEncryptionService.php
@@ -1,105 +1,56 @@
 <?php
 
-namespace FnService\JWT;
+namespace FunctionalCoding\JWT;
 
-use Exception;
-use FnService\Service;
-use Jose\Component\Core\AlgorithmManager;
-use Jose\Component\Core\JWK;
-use Jose\Component\Encryption\Algorithm\ContentEncryption\A128CBCHS256;
-use Jose\Component\Encryption\Algorithm\KeyEncryption\A128GCMKW;
-use Jose\Component\Encryption\Compression\CompressionMethodManager;
-use Jose\Component\Encryption\Compression\Deflate;
-use Jose\Component\Encryption\JWEBuilder;
-use Jose\Component\Encryption\Serializer\CompactSerializer;
+use FunctionalCoding\Service;
+use JOSE_JWE;
 
 class TokenEncryptionService extends Service
 {
-    public static function getArrBindNames() : array
+    public static function getArrBindNames()
     {
         return [];
     }
 
-    public static function getArrCallbacks() : array
+    public static function getArrCallbacks()
     {
         return [];
     }
 
-    public static function getArrLoaders() : array
+    public static function getArrLoaders()
     {
         return [
-            'encrypter' => function () {
-
-                $keyEncryptionAlgorithmManager = new AlgorithmManager([
-                    new A128GCMKW,
-                ]);
-                $contentEncryptionAlgorithmManager = new AlgorithmManager([
-                    new A128CBCHS256,
-                ]);
-                $compressionMethodManager = new CompressionMethodManager([
-                    new Deflate,
-                ]);
-
-                return new JWEBuilder(
-                    $keyEncryptionAlgorithmManager,
-                    $contentEncryptionAlgorithmManager,
-                    $compressionMethodManager
-                );
-            },
-
-            'jwk' => function () {
-
-                return new JWK([
-                    'alg'
-                        => 'A128GCMKW',
-                    'use'
-                        => 'enc',
-                    'kty'
-                        => 'oct',
-                    'k'
-                        => 'I2FeeR3Th6FmhgHN-cxd-9GRRiwcNB2OQzW6vouGFd5hcAwNAu1377hvDmGLKttBitlHiFzk643FyHw4XFM9tdJ90s2zmkX3SsE2KX5B1Qe_sEhqYmWZsJyjeyx-Q0w4B2jX7b39GUybimHUoVHDPTUrgPUKeBf-xVIGJCvHyiE'
-                ]);
-            },
-
             'payload' => function () {
-
-                throw new Exception;
+                throw new \Exception();
             },
 
             'result' => function ($token) {
-
                 return $token;
             },
 
-            'token' => function ($encrypter, $jwk, $payload) {
+            'token' => function ($key, $payload) {
+                $jwe = new JOSE_JWE(json_encode($payload));
 
-                $jwe = $encrypter
-                    ->create()
-                    ->withPayload(json_encode($payload))
-                    ->withSharedProtectedHeader([
-                        'alg' => 'A128GCMKW',
-                        'enc' => 'A128CBC-HS256',
-                        'zip' => 'DEF'
-                    ])
-                    ->addRecipient($jwk)
-                    ->build();
-
-                return (new CompactSerializer)->serialize($jwe, 0);
+                return $jwe->encrypt($key);
             },
         ];
     }
 
-    public static function getArrPromiseLists() : array
+    public static function getArrPromiseLists()
     {
         return [];
     }
 
-    public static function getArrRuleLists() : array
+    public static function getArrRuleLists()
     {
-        return [];
+        return [
+            'key' => ['required', 'string'],
+
+            'payload' => ['required', 'array'],
+        ];
     }
 
-    public static function getArrTraits() : array
+    public static function getArrTraits()
     {
         return [];
     }
